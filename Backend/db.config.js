2ffa4dcc2552253bdadcfe0408554ae271a1102c
Kevinkumar Patel <69052262+kevu4567@users.noreply.github.com>
2023-11-20 18:23:25 -0600

    Dhruv helped

---
+++ Backend/db.config.js
@@ -0,0 +1,9 @@
+// db.config.js
+require("dotenv").config();
+
+module.exports = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+};

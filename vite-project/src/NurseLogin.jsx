4e8eb50e3c28fbede7ab60bcb4d891d5773ee0c2
Kevinkumar Patel <69052262+kevu4567@users.noreply.github.com>
2023-12-04 20:44:06 -0600

    Updated NurseLogin.jsx

--- vite-project/src/NurseLogin.jsx
+++ vite-project/src/NurseLogin.jsx
@@ -4,47 +4,43 @@ import { useNavigate } from "react-router-dom";
 import "./NurseLogin.css"; // make sure to create this CSS file
 
 const NurseLogin = () => {
-
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
- const handleLogin = async (event) => {
-   event.preventDefault();
-   try {
-     const response = await fetch("http://localhost:3000/Nurses/login", {
-       method: "POST",
-       headers: {
-         "Content-Type": "application/json",
-       },
-       body: JSON.stringify({ username, password }),
-     });
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/Nurses/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
 
-     const data = await response.json();
-     if (response.ok && data.message === "Login successful") {
-       console.log("Login successful with data:", data);
-       localStorage.setItem("token", data.token); // if token is part of your response
-       localStorage.setItem("nurseId", data.user.id); // Store the nurse ID
-       navigate("/nurse-home"); // Make sure this path matches your NurseHome route
-     } else {
-       alert(data.error || "Login failed. Please check your username and password.");
-     }
-   } catch (err) {
-     alert("An error occurred while logging in: " + err.message);
-     console.error(err);
-   }
- };
-
-
-  useEffect(() => {
-    fetch("http://localhost:3000/Nurses")
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
-  }, []);
+      const data = await response.json();
+      if (response.ok) {
+        if (data.user && data.user.EmployeeID) {
+          localStorage.setItem("nurseID", data.user.EmployeeID);
+          localStorage.setItem("token", data.token); // If your backend sends a token
+          navigate("/nurse-home");
+        } else {
+          console.error("No user data received");
+          alert("Login failed. Please try again.");
+        }
+      } else {
+        alert("Login failed. Please check your username and password.");
+        alert(`Login failed: ${data.error}`);
+      }
+    } catch (err) {
+      alert("An error occurred while logging in: " + err.message);
+      console.error(err);
+    }
+  };
 
   return (
- <div className="login-container">
+    <div className="login-container">
       <div className="login-form">
         <h2 className="login-heading">Nurse Login</h2>
         <form onSubmit={handleLogin}>
